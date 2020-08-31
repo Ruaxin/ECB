3f8c75e617231dc648d9a5e784934c8e14b2718c
Ruaxin <qy.xin@foxmail.com>
2020-08-31 11:07:45 +0800

    配置初始化

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Button, Select } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Select)
 
 new Vue({
   router,
