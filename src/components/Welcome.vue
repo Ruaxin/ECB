79554c894db58bbc8f3ad0faefafe318c2c3daf8
Ruaxin <qy.xin@foxmail.com>
2020-09-01 17:20:46 +0800

    新增 Welcome页面

---
+++ src/components/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Welcome</h3>
+    <img src="../assets/MrQuin.jpg" alt="MrQuin" width="180">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Welcome'
+  }
+</script>
+
+<style scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,21 @@ import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
 import Login from '@/components/Login.vue';
 import Home from '@/components/Home.vue';
+import Welcome from '@/components/Welcome.vue';
+import {componentFactory} from 'vue-class-component/lib/component';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {path: '/', redirect: '/login'}, // 重定向
   {path: '/login', component: Login},
-  {path: '/home', component: Home}
+  {
+    path: '/home', component: Home, redirect: '/welcome', children: [
+      {
+        path: '/welcome', component: Welcome
+      }
+    ]
+  }
 ];
 
 const router = new VueRouter({
