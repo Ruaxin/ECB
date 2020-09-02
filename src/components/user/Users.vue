2d65ea51c505575652c7d623c9d3dc2a525d95c0
Ruaxin <qy.xin@foxmail.com>
2020-09-02 16:49:16 +0800

    添加表单的渲染

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -96,10 +96,27 @@
     </el-card>
     <!--添加用户对话框-->
     <el-dialog
-      title="提示"
+      title="添加用户"
       :visible.sync="addDialogVisible"
       width="50%">
-      <span>这是一段信息</span>
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button @click="addDialogVisible = false">取 消</el-button>
     <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
@@ -122,7 +139,55 @@
         },
         userList: [],
         total: 0,
-        addDialogVisible: false
+        addDialogVisible: false,
+        addForm: {
+          username: '',
+          password: '',
+          email: '',
+          mobile: ''
+        },
+        addFormRules: {
+          username: [
+            {
+              required: true,
+              message: '请输入用户名',
+              trigger: 'blur'
+            },
+            {
+              min: 3,
+              max: 10,
+              message: '长度在 3 到 10 个字符',
+              trigger: 'blur'
+            }
+          ],
+          password: [
+            {
+              required: true,
+              message: '请输入登录密码',
+              trigger: 'blur'
+            },
+            {
+              min: 6,
+              max: 15,
+              message: '长度在 6 到 15 个字符',
+              trigger: 'blur'
+            }
+          ],
+          email: [
+            {
+              required: true,
+              message: '请输入邮箱',
+              trigger: 'blur'
+            }
+          ],
+          mobile: [
+            {
+              required: true,
+              message: '请输入手机号',
+              trigger: 'blur'
+            }
+          ]
+        }
       }
     },
     created () {
