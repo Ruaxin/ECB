4e120b42fa7b3d16a910b208fc5a2947a4afaf29
Ruaxin <qy.xin@foxmail.com>
2020-09-09 14:34:05 +0800

    三级权限渲染

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -18,7 +18,8 @@
       <el-table :data="roleList" border stripe>
         <el-table-column type="expand">
           <template v-slot="scope">
-            <el-row :class="['bdbottom',i1 === 0 ? 'bdtop' : '']" v-for="(item1,i1) in scope.row.children"
+            <el-row :class="['bdbottom',i1 === 0 ? 'bdtop' : '']"
+                    v-for="(item1,i1) in scope.row.children"
                     :key="item1.id">
               <!--              渲染一级权限-->
               <el-col :span="5">
@@ -27,12 +28,20 @@
               </el-col>
               <!--              渲染二级和三级权限-->
               <el-col :span="19">
-                <el-row :class="[i2 === 0 ? '' : 'bdtop']" v-for="(item2,i2) in item1.children" :key="item2.id">
+                <el-row :class="[i2 === 0 ? '' : 'bdtop']"
+                        v-for="(item2,i2) in item1.children"
+                        :key="item2.id">
                   <el-col :span="6">
                     <el-tag type="success">{{item2.authName}}</el-tag>
                     <i class="el-icon-caret-right"></i>
                   </el-col>
-                  <el-col :span="6">10</el-col>
+                  <el-col :span="18">
+                    <el-tag type="warning"
+                            v-for="(item3) in item2.children"
+                            :key="item3.id">
+                      {{item3.authName}}
+                    </el-tag>
+                  </el-col>
                 </el-row>
               </el-col>
             </el-row>
