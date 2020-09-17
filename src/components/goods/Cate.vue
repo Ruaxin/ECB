2b1a67d212500dfdd16e1c8b0076c09d0044bb23
Ruaxin <qy.xin@foxmail.com>
2020-09-17 15:11:52 +0800

    实现商品分类功能

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -62,8 +62,13 @@
     <el-dialog
       title="添加分类"
       :visible.sync="addCateDialogVisible"
+      @close="addCateDialogClose"
       width="50%">
-      <el-form :rules="addCateFormRules" :model="addCateForm" ref="addCateFormRef" label-width="100px">
+      <el-form
+        :rules="addCateFormRules"
+        :model="addCateForm"
+        ref="addCateFormRef"
+        label-width="100px">
         <el-form-item label="分类名称：" prop="cat_name">
           <el-input v-model="addCateForm.cat_name"></el-input>
         </el-form-item>
@@ -79,7 +84,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addCateDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCateDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -188,7 +193,34 @@
         }
       },
       parentCateChange () {
-        console.log(111)
+        if (this.selectedKeys.length > 0) {
+          this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1]
+          this.addCateForm.cat_level = this.selectedKeys.length
+        } else {
+          this.addCateForm.cat_pid = 0
+          this.addCateForm.cat_level = 0
+        }
+      },
+      addCate () {
+        this.$refs.addCateFormRef.validate(async valid => {
+          if (valid) {
+            const { data: res } = await this.$http.post('categories', this.addCateForm)
+            if (res.meta.status === 201) {
+              this.$message.success('商品分类创建成功')
+              this.getCateList()
+              this.addCateDialogVisible = false
+            } else {
+              this.$message.error('商品分类创建失败')
+              this.addCateDialogVisible = false
+            }
+          }
+        })
+      },
+      addCateDialogClose () {
+        this.$refs.addCateFormRef.resetFields()
+        this.addCateForm.cat_pid = 0
+        this.addCateForm.cat_level = 0
+        this.selectedKeys = []
       }
     }
   }
