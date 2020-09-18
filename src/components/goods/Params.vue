beba85fdd4dbeebdb40f21fbc15ff7572fa407d1
Ruaxin <qy.xin@foxmail.com>
2020-09-18 14:03:19 +0800

    修复git bug

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!--面包屑导航-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Params'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
