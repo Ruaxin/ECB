fa71000b9e2fe2a01b7727ff1a9f1ee02310e000
Ruaxin <qy.xin@foxmail.com>
2020-10-09 16:22:39 +0800

    订单列表初步渲染

---
+++ src/components/order/Order.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <!--面包屑导航-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"/>
+          </el-input>
+        </el-col>
+      </el-row>
+      <el-table
+        :data="orderList"
+        stripe
+        border>
+        <el-table-column type="index" label="#"/>
+        <el-table-column prop="order_number" label="订单编号"/>
+        <el-table-column prop="order_price" label="订单价格"/>
+        <el-table-column prop="pay_status" label="是否付款"/>
+        <el-table-column prop="is_send" label="是否发货">
+          <template v-slot="scope">
+            <el-tag type="success" v-if="scope.row.pay_status === '1'">已付款</el-tag>
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="create_time" label="下单时间">
+          <template v-slot="scope">
+            {{ scope.row.create_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini"/>
+            <el-button type="success" icon="el-icon-location" size="mini"/>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Order',
+  data () {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      total: 0,
+      orderList: [],
+    }
+  },
+  created () {
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList () {
+      const { data: res } = await this.$http.get('orders', { params: this.queryInfo })
+      if (res.meta.status === 200) {
+        this.total = res.data.total
+        this.orderList = res.data.goods
+      } else {
+        this.$message.error('')
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
