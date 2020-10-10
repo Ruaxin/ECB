97a935b3098e4b61be5be9659d681fc0885a4cdb
Ruaxin <qy.xin@foxmail.com>
2020-10-10 12:58:08 +0800

    用nprogress实现页面加载进度条

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import Vue from 'vue';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
 // 请求拦截器
 axios.interceptors.request.use(config => {
   // 对请求头做处理，在Authorization添加token,来获取权限
+  NProgress.start();
   config.headers.Authorization = window.sessionStorage.getItem('token');
   // 在最后必须 return config
+  return config;
+});
+axios.interceptors.response.use(config => {
+  NProgress.done();
   return config;
 });
 Vue.prototype.$http = axios; //全局挂载
