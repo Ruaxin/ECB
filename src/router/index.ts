9d8a27a3c2f1179a805079afd7597794c82f34c2
Ruaxin <qy.xin@foxmail.com>
2020-10-10 12:37:16 +0800

    完善图表结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,7 @@ import Params from '@/components/goods/Params.vue';
 import List from '@/components/goods/List.vue';
 import Add from '@/components/goods/Add.vue';
 import Order from '@/components/order/Order.vue';
+import Report from '@/components/report/Report.vue';
 
 Vue.use(VueRouter);
 
@@ -28,6 +29,7 @@ const routes: Array<RouteConfig> = [
       {path: '/goods', component: List},
       {path: '/goods/add', component: Add},
       {path: '/orders', component: Order},
+      {path: '/reports', component: Report},
     ]
   }
 ];
