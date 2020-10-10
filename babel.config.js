fce9ae681e628d3a7158d79b97fac2367fb79f3f
Ruaxin <qy.xin@foxmail.com>
2020-10-10 13:18:48 +0800

    配置开发阶段和发布阶段的plugin

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
+// 发布阶段
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+// 开发阶段
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: ["transform-remove-console"]
+  plugins: [
+    ...prodPlugins
+  ]
 }
